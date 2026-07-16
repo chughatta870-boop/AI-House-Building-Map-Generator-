d0eeabe8d9ac02296fa084d07d548d3cc98af880
chughatta870-boop <chughatta870@gmail.com>
2026-07-16 06:51:34 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,138 @@
+/* ==========================================
+   AI House Building Map Generator PWA
+   Service Worker
+========================================== */
+
+
+const CACHE_NAME = 
+"ai-house-map-v1";
+
+
+const FILES_TO_CACHE = [
+
+    "./",
+
+    "./index.html",
+
+    "./style.css",
+
+    "./script.js",
+
+    "./manifest.json",
+
+    "./icons/icon-192.png",
+
+    "./icons/icon-512.png"
+
+];
+
+
+
+/* ---------- INSTALL ---------- */
+
+
+self.addEventListener(
+"install",
+event=>{
+
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache=>{
+
+
+            return cache.addAll(
+                FILES_TO_CACHE
+            );
+
+
+        })
+
+    );
+
+
+    self.skipWaiting();
+
+
+});
+
+
+
+/* ---------- ACTIVATE ---------- */
+
+
+self.addEventListener(
+"activate",
+event=>{
+
+
+    event.waitUntil(
+
+        caches.keys()
+
+        .then(keys=>{
+
+
+            return Promise.all(
+
+                keys.map(key=>{
+
+
+                    if(
+                    key !== CACHE_NAME
+                    ){
+
+                        return caches.delete(key);
+
+                    }
+
+
+                })
+
+            );
+
+
+        })
+
+    );
+
+
+    self.clients.claim();
+
+
+});
+
+
+
+/* ---------- FETCH ---------- */
+
+
+self.addEventListener(
+"fetch",
+event=>{
+
+
+    event.respondWith(
+
+        caches.match(
+            event.request
+        )
+
+        .then(response=>{
+
+
+            return response ||
+
+            fetch(
+                event.request
+            );
+
+
+        })
+
+    );
+
+
+});
